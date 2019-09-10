f5b1a429696f7545a213793f758af49c98960b59
Christopher Ruiz <chrisruizaz@gmail.com>
2019-09-10 16:54:39 -0700

    added the gif too the api and got it to fire broken images

--- gifTastic.js
+++ gifTastic.js
@@ -1,6 +1,6 @@
 console.log("Im attached");
 //I want to create an array of buttons
-var gifButtons = [`r2d2`, `c3po`, `solo`, `chewie`, `luke`];
+var gifButtons = [`r2d2`];
 //I want the buttons to show up under the search bar
 // $(".container").append(gifButtons);
 //buttons should contain what was searched
@@ -30,15 +30,29 @@ renderButtons();
 //buttons will contain gifs from api
 //
 ////Ajax call to the API
-var queryURL =
-  `https://api.giphy.com/v1/gifs/` +
-  gifButtons +
-  `?api_key=SBDVZzF2iQmWatKX1P3LaBWWkEjJnb7D&q=r2d2&limit=25&offset=0&rating=G&lang=en`;
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  $("#movie-view").text(JSON.stringify(response));
+$("#buttons-view").on("click", function() {
+  var queryURL =
+    `https://api.giphy.com/v1/gifs/search?api_key=SBDVZzF2iQmWatKX1P3LaBWWkEjJnb7D&q=` +
+    gifButtons +
+    `&limit=5&offset=0&rating=G&lang=en`;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(queryURL);
+    console.log(response);
+
+    for (i = 0; i < 5; i++) {
+      var imageUrl = response.data[i].url;
+      console.log(imageUrl);
+      var gifImage = $("<img>");
+      gifImage.attr("src", imageUrl);
+      gifImage.attr("alt", "starwars image");
+      $("#gifs-view").append(gifImage);
+      //$("#buttons-view").text(JSON.stringify(response));
+      console.log(gifImage);
+    }
+  });
 });
 //how do I get the API search through my Array in an onclick of my Gifs
 //figure out how to play and pause Gifs
