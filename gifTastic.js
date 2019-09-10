5f827557af49b729d27aaa77a93e3bf98e7deaa6
Christopher Ruiz <chrisruizaz@gmail.com>
2019-09-09 21:32:00 -0700

    Added Ajax function and URL

--- gifTastic.js
+++ gifTastic.js
@@ -3,22 +3,24 @@ console.log("Im attached");
 var gifButtons = [`r2d2`, `c3po`, `solo`, `chewie`, `luke`];
 //I want the buttons to show up under the search bar
 // $(".container").append(gifButtons);
-console.log(gifButtons);
 //buttons should contain what was searched
 function renderButtons() {
   $("#buttons-view").empty();
   for (var i = 0; i < gifButtons.length; i++) {
-    var a = $(`<button>`);
-    a.addClass(`movie`);
-    a.attr(`data-name`, gifButtons[i]);
+    var a = $("<button>");
+    a.addClass("btn btn-secondary m-4 gif");
+    a.attr("data-name", gifButtons[i]);
     a.text(gifButtons[i]);
-    $(`#buttons-view`).append(a);
+    $("#buttons-view").append(a);
+    console.log(a);
   }
 }
-$(`#add-movie`).on(`click`, function(event) {
+$("#add-gif").on("click", function(event) {
   console.log(event);
   event.preventDefault();
-  var gifSearch = $(`movie-input`).val();
+  var gifSearch = $("#search-input")
+    .val()
+    .trim();
   gifButtons.push(gifSearch);
   console.log(gifButtons);
   console.log(gifSearch);
@@ -27,3 +29,16 @@ $(`#add-movie`).on(`click`, function(event) {
 renderButtons();
 //buttons will contain gifs from api
 //
+////Ajax call to the API
+var queryURL =
+  `https://api.giphy.com/v1/gifs/` +
+  gifButtons +
+  `?api_key=SBDVZzF2iQmWatKX1P3LaBWWkEjJnb7D&q=r2d2&limit=25&offset=0&rating=G&lang=en`;
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  $("#movie-view").text(JSON.stringify(response));
+});
+//how do I get the API search through my Array in an onclick of my Gifs
+//figure out how to play and pause Gifs
