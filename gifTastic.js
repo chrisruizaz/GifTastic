acb491e2fb9688c40cee7d519fb6e02445d3490e
Christopher Ruiz <chrisruizaz@gmail.com>
2019-09-12 18:23:00 -0700

    finally got the gifs to play and still

--- gifTastic.js
+++ gifTastic.js
@@ -5,9 +5,11 @@ var gifButtons = [`r2d2`];
 // $(".container").append(gifButtons);
 //buttons should contain what was searched
 function renderButtons() {
-  $("#buttons-view").empty();
+  var a = $("<button>");
+  //
+  // $("#buttons-view").empty();
+  //
   for (var i = 0; i < gifButtons.length; i++) {
-    var a = $("<button>");
     a.addClass("btn btn-secondary m-4 gif");
     a.attr("data-name", gifButtons[i]);
     a.text(gifButtons[i]);
@@ -15,10 +17,11 @@ function renderButtons() {
   }
 }
 $("#add-gif").on("click", function(event) {
-  event.preventDefault();
   var gifSearch = $("#search-input")
     .val()
     .trim();
+  event.preventDefault();
+
   gifButtons.push(gifSearch);
 
   renderButtons();
@@ -38,40 +41,53 @@ $(document).on("click", ".gif", function() {
   $.ajax({
     url: queryURL,
     method: "GET"
+    //
+    //
   }).then(function(response) {
+    //
     console.log(response);
-    for (i = 0; i < 5; i++) {
-      var imageUrl = response.data[i].images.original.url;
-      console.log(imageUrl);
+    var results = response.data;
+    for (i = 0; i < results.length; i++) {
+      var resultDiv = $("<div>");
+      //var imageUrl = response.data[i].images.original.url;
+      //console.log(imageUrl);
       var gifImage = $("<img>");
-      gifImage.attr("src", imageUrl);
-      gifImage.attr("alt", "starwars image");
-
-      $("#gifs-view").append(gifImage);
+      //
+      gifImage.attr("src", results[i].images.fixed_height_still.url);
+      //gifImage.attr("src", results[i].images.original.url);
+      gifImage.attr("class", "starwars-gif");
+      //
+      gifImage.attr("data-state", "still"); //new
+      //
+      gifImage.attr("data-animate", results[i].images.fixed_height.url); //new
+      //gifImage.attr("data-animate", results[i].images.original.url);
+      gifImage.attr("data-still", results[i].images.fixed_height_still.url); //new
+      //gifImage.attr("data-still", results[i].images.orginal_still.url);
+      //var stillUrl = response.data[i].images.original_still.url;
+      resultDiv.append(gifImage);
+      // gifImage.attr("gif-switch", "off");
+      //gifImage.attr("still-gif", stillUrl);
+      //$("#gifs-view").append(gifImage);
       //$("#buttons-view").text(JSON.stringify(response));
+      $("#gifs-view").prepend(resultDiv);
       console.log(gifImage);
     }
-    var stillUrl = response.data[i].images.original_still.url;
-    gifImage.attr("src", stillUrl);
-    gifImage.attr("gif-switch", "off");
-    gifImage.attr("still-gif", stillUrl);
   });
 });
 //I cannot get this function to work at all. I feel like it makes sense and its right there but I just keep brea
-//$(".gif").on("click", function() {
-//gifImage.attr("gif", imageUrl);
-// The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-//var state = $(this).attr("data-state");
-// If the clicked image's state is still, update its src attribute to what its data-animate value is.
-// Then, set the image's data-state to animate
-// Else set src to the data-still value
-//if (state === "still") {
-// $(this).attr("src", $(this).attr("data-animate"));
-//$(this).attr("data-state", "animate");
-// } else {
-// $(this).attr("src", $(this).attr("data-still"));
-// $(this).attr("data-state", "still");
-// }
-//});
+$(document).on("click", ".starwars-gif", function() {
+  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  var state = $(this).attr("data-state");
+  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+  // Then, set the image's data-state to animate
+  // Else set src to the data-still value
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
 //how do I get the API search through my Array in an onclick of my Gifs
 //figure out how to play and pause Gifs
