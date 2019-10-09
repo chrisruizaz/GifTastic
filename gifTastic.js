1cc47e2e83c37f6271d460b911cbf8786c06af89
Christopher Ruiz <chrisruizaz@gmail.com>
2019-10-08 22:00:34 -0700

    refactored code moved around the actual function

--- gifTastic.js
+++ gifTastic.js
@@ -1,13 +1,8 @@
 console.log("Im attached");
 //I want to create an array of buttons
 var gifButtons = [`r2d2`];
-//I want the buttons to show up under the search bar
-// $(".container").append(gifButtons);
-//buttons should contain what was searched
 function renderButtons() {
   var a = $("<button>");
-  //
-  // $("#buttons-view").empty();
   //
   for (var i = 0; i < gifButtons.length; i++) {
     a.addClass("btn btn-secondary m-4 gif");
@@ -27,10 +22,27 @@ $("#add-gif").on("click", function(event) {
   renderButtons();
 });
 renderButtons();
-//buttons will contain gifs from api
-//
-////Ajax call to the API
-$(document).on("click", ".gif", function() {
+function displayGif(response) {
+  //
+  console.log(response);
+  var results = response.data;
+  for (i = 0; i < results.length; i++) {
+    var resultDiv = $("<div>");
+    var gifImage = $("<img>");
+    //
+    gifImage.attr("src", results[i].images.fixed_height_still.url);
+    gifImage.attr("class", "starwars-gif");
+    //
+    gifImage.attr("data-state", "still"); //new
+    //
+    gifImage.attr("data-animate", results[i].images.fixed_height.url); //new
+    gifImage.attr("data-still", results[i].images.fixed_height_still.url); //new
+    resultDiv.append(gifImage);
+    $("#gifs-view").prepend(resultDiv);
+    console.log(gifImage);
+  }
+}
+function starwarsCall() {
   var character = $(this).attr("data-name");
   var queryURL =
     `https://api.giphy.com/v1/gifs/search?api_key=SBDVZzF2iQmWatKX1P3LaBWWkEjJnb7D&q=` +
@@ -43,44 +55,11 @@ $(document).on("click", ".gif", function() {
     method: "GET"
     //
     //
-  }).then(function(response) {
-    //
-    console.log(response);
-    var results = response.data;
-    for (i = 0; i < results.length; i++) {
-      var resultDiv = $("<div>");
-      //var imageUrl = response.data[i].images.original.url;
-      //console.log(imageUrl);
-      var gifImage = $("<img>");
-      //
-      gifImage.attr("src", results[i].images.fixed_height_still.url);
-      //gifImage.attr("src", results[i].images.original.url);
-      gifImage.attr("class", "starwars-gif");
-      //
-      gifImage.attr("data-state", "still"); //new
-      //
-      gifImage.attr("data-animate", results[i].images.fixed_height.url); //new
-      //gifImage.attr("data-animate", results[i].images.original.url);
-      gifImage.attr("data-still", results[i].images.fixed_height_still.url); //new
-      //gifImage.attr("data-still", results[i].images.orginal_still.url);
-      //var stillUrl = response.data[i].images.original_still.url;
-      resultDiv.append(gifImage);
-      // gifImage.attr("gif-switch", "off");
-      //gifImage.attr("still-gif", stillUrl);
-      //$("#gifs-view").append(gifImage);
-      //$("#buttons-view").text(JSON.stringify(response));
-      $("#gifs-view").prepend(resultDiv);
-      console.log(gifImage);
-    }
-  });
-});
-//I cannot get this function to work at all. I feel like it makes sense and its right there but I just keep brea
-$(document).on("click", ".starwars-gif", function() {
-  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  }).then(displayGif);
+}
+$(document).on("click", ".gif", starwarsCall);
+$(document).on("click", ".starwars-gif", function gifAnimate() {
   var state = $(this).attr("data-state");
-  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-  // Then, set the image's data-state to animate
-  // Else set src to the data-still value
   if (state === "still") {
     $(this).attr("src", $(this).attr("data-animate"));
     $(this).attr("data-state", "animate");
@@ -89,5 +68,3 @@ $(document).on("click", ".starwars-gif", function() {
     $(this).attr("data-state", "still");
   }
 });
-//how do I get the API search through my Array in an onclick of my Gifs
-//figure out how to play and pause Gifs
